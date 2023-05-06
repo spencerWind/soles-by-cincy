0a660e2810f066013b02b29198cfdf54eb58dae7
spencerWind <swind03@gmail.com>
2023-05-06 02:23:26 -0400

    finished mobile nav ui

--- client/src/components/Navigation/HamburgerMenu.jsx
+++ client/src/components/Navigation/HamburgerMenu.jsx
@@ -1,20 +1,73 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import MobileMenu from "../../assets/mobileMenu.svg";
+import CloseMenu from "../../assets/closeMenu.svg";
+import FacebookIcon from "../../assets/facebookIcon.svg";
+import InstagramIcon from "../../assets/instagramIcon.svg";
+import TwitterIcon from "../../assets/twitterIcon.svg";
+import TikTokIcon from "../../assets/tiktokIcon.svg";
+import { motion, useAnimation } from "framer-motion";
 
 const HamburgerMenu = () => {
-    const slideOutMenu = useRef();
+    const [menuIsActive, setMenuIsActive] = useState(null);
+    const slideOutMenuAnimation = useAnimation();
+    const slideOutMenu = useRef(null);
 
-    useEffect(() =>{console.log("hello")}, [])
+    useEffect(() => {
+        menuIsActive
+            ? slideOutMenuAnimation.start({
+                  x: 0,
+                  transition: {
+                      duration: 0.5,
+                  },
+              })
+            : slideOutMenuAnimation.start({
+                  x: "100vw",
+                  transition: {
+                      type: "Inertia",
+                      duration: 0.5,
+                  },
+              });
+    }, [menuIsActive]);
 
     return (
         <section className="">
-            <button onClick={() => {console.log(slideOutMenu)}}>
+            <button
+                onClick={() => {
+                    setMenuIsActive(true);
+                }}>
                 <img
                     src={MobileMenu}
                     alt="Mobile Menu"
                 />
             </button>
-            <section ref={slideOutMenu} className="fixed w-screen border top-0 left-0 h-screen -z-10"></section>
+            <motion.section
+                ref={slideOutMenu}
+                animate={slideOutMenuAnimation}
+                className="fixed w-screen top-0 right-0 h-screen bg-themeRed p-5 flex flex-col justify-between items-center">
+                <button
+                    className="self-start"
+                    onClick={() => {
+                        setMenuIsActive(false);
+                    }}>
+                    <img
+                        src={CloseMenu}
+                        alt="Close Menu"
+                    />
+                </button>
+                <ul className="text-4xl uppercase font-black tracking-widest text-themeGray flex flex-col items-center gap-10">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Gallery</li>
+                    <li>Shop</li>
+                </ul>
+                <ul className="h-[48px] w-full flex justify-around items-center">
+                    <li><img src={FacebookIcon} alt="Facebook Link" /></li>
+                    <li><img src={InstagramIcon} alt="Instagram Link" /></li>
+                    <li><img src={TwitterIcon} alt="Twitter Link" /></li>
+                    <li><img src={TikTokIcon} alt="TikTok Link" /></li>
+
+                </ul>
+            </motion.section>
         </section>
     );
 };
--- client/src/components/Navigation/NavBar.jsx
+++ client/src/components/Navigation/NavBar.jsx
@@ -2,7 +2,7 @@ import HamburgerMenu from "./HamburgerMenu"
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-between items-center p-5">
+    <nav className="flex justify-between items-center p-5 bg-themeGray">
         <h1 className="text-2xl font-bold leading-none">Soles By Cincy</h1>
         <HamburgerMenu />
     </nav>
